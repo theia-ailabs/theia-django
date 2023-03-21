7a71bf058d368739210f4dee8d848c505688473b
quantium <alexcollsoutumuro@gmail.com>
2023-03-21 16:30:00 +0100

    add davinci app route

--- src/server/urls.py
+++ src/server/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('welcome/', include('welcome.urls')),
-    # path('davinci/', include('davinci.urls')),
+    path('davinci/', include('davinci.urls')),
     path('admin/', admin.site.urls),
 ]
