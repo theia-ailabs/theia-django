779d6ce3a651107cebf0fe3ddf55569b98638e8d
quantium <alexcollsoutumuro@gmail.com>
2023-03-25 18:30:07 +0100

    add spider.py

---
+++ src/services/spider.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+r = requests.get(
+    'https://www.fotocasa.es/es/alquiler/viviendas/cornella-de-llobregat/todas-las-zonas/l')
+soup = BeautifulSoup(r.text, 'lxml')
+
+# print(r.text)
+
+print(soup)
